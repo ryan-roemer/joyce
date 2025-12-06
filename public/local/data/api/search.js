741b56ebc6ef2a459b5d6666c71b302d766ef06d
Ryan Roemer <ryan@loose-bits.com>
2025-12-05 21:25:45 -0800

    Notes

--- public/local/data/api/search.js
+++ public/local/data/api/search.js
@@ -3,6 +3,8 @@ import { create, insertMultiple } from "@orama/orama";
 import { getAndCache } from "../../../shared-util.js";
 import { getPosts } from "./posts.js";
 
+// TODO(EMBEDDINGS): Split the script to do by chunks.
+// TODO(EMBEDDINGS): Revise the scheme to handle chunks...
 export const getDb = getAndCache(async () => {
   const postsObj = await getPosts();
   const posts = Object.values(postsObj);
@@ -24,6 +26,8 @@ export const getDb = getAndCache(async () => {
   });
 
   await insertMultiple(db, posts);
+
+  console.log("TODO ORAMA", db.data.docs.docs); // eslint-disable-line no-undef
 
   return db;
 });
