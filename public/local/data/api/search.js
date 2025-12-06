e3f068be9ee25868bffa38e36fe57cb1d02d8172
Ryan Roemer <ryan@loose-bits.com>
2025-12-06 15:31:25 -0800

    Now have two databases.

--- public/local/data/api/search.js
+++ public/local/data/api/search.js
@@ -1,11 +1,10 @@
 import { create, insertMultiple } from "@orama/orama";
 
 import { getAndCache } from "../../../shared-util.js";
-import { getPosts } from "./posts.js";
+import { getPosts, getPostsEmbeddings } from "./posts.js";
 
-// TODO(EMBEDDINGS): Split the script to do by chunks.
-// TODO(EMBEDDINGS): Revise the scheme to handle chunks...
-export const getDb = getAndCache(async () => {
+// Posts database (full-text search)
+export const getPostsDb = getAndCache(async () => {
   const postsObj = await getPosts();
   const posts = Object.values(postsObj);
 
@@ -27,7 +26,46 @@ export const getDb = getAndCache(async () => {
 
   await insertMultiple(db, posts);
 
-  console.log("TODO ORAMA", db.data.docs.docs); // eslint-disable-line no-undef
+  return db;
+});
+
+// Chunks database (vector search)
+export const getChunksDb = getAndCache(async () => {
+  const embeddingsObj = await getPostsEmbeddings();
+
+  // Flatten chunks: each chunk becomes a document with slug reference
+  const chunks = Object.entries(embeddingsObj).flatMap(([slug, { chunks }]) =>
+    chunks.map((chunk) => ({
+      slug,
+      start: chunk.start,
+      end: chunk.end,
+      embeddings: chunk.embeddings,
+    })),
+  );
+
+  const db = await create({
+    schema: {
+      slug: "string",
+      start: "number",
+      end: "number",
+      embeddings: "vector[384]",
+    },
+  });
+
+  await insertMultiple(db, chunks);
+
+  return db;
+});
+
+export const getDb = getAndCache(async () => {
+  const [postsDb, chunksDb] = await Promise.all([getPostsDb(), getChunksDb()]);
+
+  const db = {
+    posts: postsDb,
+    chunks: chunksDb,
+  };
+
+  console.log("TODO ORAMA", db); // eslint-disable-line no-undef
 
   return db;
 });
